5382bfc718e6d9e52a3dde96903cca441edf7a7e
Dauren-Delmukhambetov <dauren.del@gmail.com>
2022-02-15 18:26:57 +0300

    Refactoring handler to support API Gateway Proxy events

--- src/main/java/org/serverless/umbrella/Handler.java
+++ src/main/java/org/serverless/umbrella/Handler.java
@@ -2,13 +2,22 @@ package org.serverless.umbrella;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 import static java.lang.String.format;
+import static java.util.Collections.singletonMap;
 
-public class Handler implements RequestHandler<String, String> {
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     @Override
-    public String handleRequest(String input, Context context) {
-        return format("Hello, %s!", input);
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+
+        final var greetedPerson = input.getBody();
+
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(200)
+                .withHeaders(singletonMap("Content-Type", "application/json"))
+                .withBody(format("Hello, %s!", greetedPerson));
     }
 }
