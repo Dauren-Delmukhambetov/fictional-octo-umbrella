d20db6309f25e5ff6f775614a30036fd2bf82599
Dauren Delmukhambetov <dauren.del@gmail.com>
2022-02-18 12:23:05 +0300

    Refactoring handler to support API Gateway Proxy events (#6)

--- src/test/java/org/serverless/umbrella/HandlerTest.java
+++ src/test/java/org/serverless/umbrella/HandlerTest.java
@@ -1,16 +1,22 @@
 package org.serverless.umbrella;
 
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HandlerTest {
 
-    private Handler handler = new Handler();
+    private final Handler handler = new Handler();
 
     @Test
     void shouldReturnGreeting() {
-        final var actual = handler.handleRequest("Adam Smith", null);
-        assertEquals( "Hello, Adam Smith!", actual);
+        final var input = new APIGatewayProxyRequestEvent()
+                .withBody("Adam Smith");
+
+        final var actual = handler.handleRequest(input, null);
+
+        assertEquals( 200, actual.getStatusCode());
+        assertEquals( "Hello, Adam Smith!", actual.getBody());
     }
 }
