0215fd23e530c3b7ee02ee32f794d5a36fb37187
Dauren Delmukhambetov <dauren.del@gmail.com>
2022-12-20 11:14:36 +0300

    Introducing stages for API Gateway (#42)

--- src/main/java/org/serverless/umbrella/EmailMasker.java
+++ src/main/java/org/serverless/umbrella/EmailMasker.java
@@ -4,6 +4,8 @@ import net.datafaker.Faker;
 
 import java.util.regex.Pattern;
 
+import static java.lang.String.format;
+
 public class EmailMasker {
 
     private static final Pattern EMAIL_PATTERN = Pattern.compile("([0-9A-Za-z-\\._])+@([0-9A-Za-z])+[\\.]?([0-9A-Za-z]){1,5}");
@@ -11,6 +13,10 @@ public class EmailMasker {
     private static final Faker faker = new Faker();
 
     public static String mask(final String data) {
-        return data.replaceAll(EMAIL_PATTERN.pattern(), faker.name().username() + "@" + faker.internet().domainName());
+        return data.replaceAll(EMAIL_PATTERN.pattern(), fakeEmail()) + " [from production]";
+    }
+
+    private static String fakeEmail() {
+        return format("%s@%s", faker.name().username(), faker.internet().domainName());
     }
 }
